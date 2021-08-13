d900778f7be4b9ca61d865ea69941ea343cefc7e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2021-08-13 22:54:05 +0900

    set up tailwind build & postcss-import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
+let cjkFallback = [
+  "Noto Sans CJK TC",
+  "Microsoft Jhenghei",
+  "Microsoft Yahei",
+  "Meiryo",
+  "Malgun Gothic",
+];
+
 module.exports = {
   purge: ["./layouts/**/*.html"],
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["M PLUS 1p", ...cjkFallback, "sans-serif"],
+      // serif: ["Equity"],
+    },
     extend: {
       // https://github.com/tailwindlabs/tailwindcss/discussions/1361
       colors: {
         inherit: "inherit",
+        a: "#e1c2bd",
+        b: "#91d6c3",
+        white: "#f8f6f7",
+        black: "#000000",
       },
     },
   },
