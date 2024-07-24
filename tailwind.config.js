29a1679bc135b7c2625c43d7a2e18a334bac285a
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-07-24 17:42:09 +0900

    import 20180316

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,8 @@ module.exports = {
     },
     extend: {
       fontSize: {
-        xs: ["12px", { lineHeight: "1rem" }],
-        sm: ["14px", { lineHeight: "1.25rem" }],
+        xs: ["14px", { lineHeight: "1rem" }],
+        sm: ["16px", { lineHeight: "1.4" }],
         base: ["18px", { lineHeight: "1.6" }],
         lg: ["20px", { lineHeight: "1.4" }],
         xl: ["24px", { lineHeight: "1.4" }],
